47e8fc7abc2edc5b887439033a0691e3b575a153
luigimagdamit <luigiantonmagdamit@gmail.com>
2024-10-18 01:37:04 -0700

    basic stdout tests

---
+++ ube_test.go
@@ -0,0 +1,65 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"os/exec"
+	"testing"
+)
+
+func TestHelloWorld(t *testing.T) {
+	// Run the command
+	cmd := exec.Command("./ubescript", "./tests/hello.ube")
+	output, err := cmd.Output()
+	if err != nil {
+		t.Fatalf("Error executing command: %v", err)
+	}
+
+	// Write the command output to a file
+	err = ioutil.WriteFile("output.txt", output, 0644)
+	if err != nil {
+		t.Fatalf("Error writing to file: %v", err)
+	}
+
+	// Read the output file
+	data, err := ioutil.ReadFile("output.txt")
+	if err != nil {
+		t.Errorf("Error reading file: %v", err)
+	}
+
+	expected := "hello world luigi\n"
+	if string(data) != expected {
+		t.Errorf("Expected %q, got %q", expected, string(data))
+	}
+
+	// Print the file content
+	fmt.Println(string(data))
+}
+func TestFib(t *testing.T) {
+	// Run the command
+	cmd := exec.Command("./ubescript", "./tests/fib.ube")
+	output, err := cmd.Output()
+	if err != nil {
+		t.Fatalf("Error executing command: %v", err)
+	}
+
+	// Write the command output to a file
+	err = ioutil.WriteFile("output.txt", output, 0644)
+	if err != nil {
+		t.Fatalf("Error writing to file: %v", err)
+	}
+
+	// Read the output file
+	data, err := ioutil.ReadFile("output.txt")
+	if err != nil {
+		t.Errorf("Error reading file: %v", err)
+	}
+
+	expected := "0\n1\n1\n2\n3\n5\n8\n"
+	if string(data) != expected {
+		t.Errorf("Expected %q, got %q", expected, string(data))
+	}
+
+	// Print the file content
+	fmt.Println(string(data))
+}
